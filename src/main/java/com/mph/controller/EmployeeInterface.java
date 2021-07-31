92f8414e28760c558da195e14ea54389d7e4a283
va-cell <vaishnavilakshmi29@gmail.com>
2021-07-31 12:27:30 -0700

    employee controller added

--- src/main/java/com/mph/controller/EmployeeInterface.java
+++ src/main/java/com/mph/controller/EmployeeInterface.java
@@ -1,6 +1,19 @@
-package employeee;
+package com.mph.controller;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface EmployeeInterface {
-	public void add();
-
+	public List addEmployee();
+	public void viewEmployee(List elist);
+	public void serializeEmp(List elist ) ;
+	public void deSerializeEmp(List elist) ;
+	public void SortByEmpName(List elist);
+	public void ViewByDepartment(List elist);
+	public void InsertUsingProcedure();
+	public void rsmd();
+	public void type_forward_only_rs();
+	public void type_scroll_insensitive_rs();
+	public void type_scroll_sensitive_rs();
+	public void type_scroll_sensitive_update_rs();
 }
