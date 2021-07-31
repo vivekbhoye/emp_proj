daccf61f722d78d901d54efe3638815d560f78ad
vaibhav <vaibhav77bhatia@gmail.com>
2021-07-31 12:33:57 +0530

    all perfect

---
+++ src/main/java/com/mph/Util2/DBConnection.java
@@ -0,0 +1,30 @@
+package com.mph.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	static Connection con;
+	public static Connection getDBConnection()
+	{
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			 con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","1234");
+			 System.out.println(con);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return con;
+	}
+		
+	
+	
+	public static void main(String[] args) {
+		getDBConnection();
+	}
+
+}
